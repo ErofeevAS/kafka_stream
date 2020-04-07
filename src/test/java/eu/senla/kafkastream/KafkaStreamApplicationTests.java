4494a336e7c83a656a3b19c247e6a625a2f432a0
ErofeevAS <erofeevas152@gmail.com>
2020-04-07 16:26:41 +0300

    first commit

---
+++ src/test/java/eu/senla/kafkastream/KafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.senla.kafkastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
